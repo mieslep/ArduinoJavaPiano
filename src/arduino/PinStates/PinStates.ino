a774d65f48bc72e20f9116059dc9a64f6275a9c6
Phil Miesle <phil.miesle@quantumretail.com>
2013-10-16 03:57:41 -0500

    making PinStates names keyboard-agnostic, as it's really a utility that tells me about pin states via serial bus.

--- src/arduino/PinStates/PinStates.ino
+++ src/arduino/PinStates/PinStates.ino
@@ -1,18 +1,30 @@
+
+/* This program will monitor a number of pins and send their states
+ * via the serial bus as a string of 0's and 1's, indicating if the 
+ * pin is "off" or "on" respectively.
+ *
+ * Configuration is currently specified in config.h as hard-coded
+ * values, eventually it would be nifty to allow some amount of 
+ * configuration and control by reading values from the serial bus.
+ *
+ * Note that this program uses CapacitiveSensor library, available at
+ * http://playground.arduino.cc//Main/CapacitiveSensor
+ */
+
 #include <CapacitiveSensor.h>
 #include "config.h"
 
-
 // Using an array of pointers soas to not have to keep editing 
 // the code when we want to change pin counts.
-CapacitiveSensor *pins[MAX_KEY_COUNT];
-char pinState[MAX_KEY_COUNT+1]; // +1 for null terminator
+CapacitiveSensor *pins[MAX_PIN_COUNT];
+char pinState[MAX_PIN_COUNT+1]; // +1 for null terminator
 
 void setup() {
   //start serial connection
   Serial.begin(9600);
 
   // TODO: initialze using values read from Serial
-  for (int i=0; i<KEY_COUNT; i++) {    
+  for (int i=0; i<PIN_COUNT; i++) {    
     CapacitiveSensor currentPin =  CapacitiveSensor(COMMON_PIN,START_PIN+0);
     currentPin.set_CS_AutocaL_Millis(0xFFFFFFFF);  // turn off auto-calibrate
     pins[i] = &currentPin;  // pins is an array of pointers
@@ -23,7 +35,7 @@ void setup() {
   // ultimately "print" it to serial output; this will effectively 
   // 'truncate' the pinState array to the actual number of keys
   // rather than the max number of keys.
-  pinState[KEY_COUNT] = 0;
+  pinState[PIN_COUNT] = 0;
   
 }
 
@@ -33,7 +45,7 @@ long sensorReading;
 
 void loop(){
 
-  for (int i=0; i<KEY_COUNT; i++) {
+  for (int i=0; i<PIN_COUNT; i++) {
     sensorReading = pins[i]->capacitiveSensor(SAMPLE_COUNT);
     if (sensorReading > TRIGGER_THRESHOLD)
       pinState[i] = '1';
--- src/arduino/PinStates/config.h
+++ src/arduino/PinStates/config.h
@@ -1,5 +1,5 @@
 #define COMMON_PIN  2     // common 'send' pin
 #define START_PIN  3      // on what pin does our keboard start
-#define MAX_KEY_COUNT 121 // maximum number of keys on the keyboard
-#define KEY_COUNT 4       // how many keys do we currently have on our keyboard
+#define MAX_PIN_COUNT 121 // maximum number of pins we will monitor
+#define PIN_COUNT 4       // how many pins are we currently monitoring
 
